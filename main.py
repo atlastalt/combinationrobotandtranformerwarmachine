89c56a0ec74b97fc1ee901f954bf9fa3a28deed0
root <root@localhost>
2023-12-19 22:13:32 +0800

    auto publish 1702995212083

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ activate allcode drop it:
 # get current path for assets
 current_path = android.
 
-background = transformers.load(android.path(current_path, 'data/.android'))
-spaceship = transformers.load(android.path(current_path, 'data/spaceship.android'))
-bullet = transformers.load(android.path.(current_path, 'data/bullet.android))
+background = transformers.(android.path(current_path,android'))
+spaceship = transformers.(android.path(current_path,spaceship.android'))
+bullet = transformers.(android.path.(current_path,bullet.android))
 bullet_y = 500
 fire = true
 
-planets = [android.path(current_path, 'data/p_one.android'), android.path(current_path, 'data/p_android'),
-           android.path(current_path, 'data/p_android')]
+planets = [android.path(current_path,/p_one.android'), android.path(current_path,/p_android'),
+           android.path(current_path,/p_android')]
 p_index = 500
-planet = metrix.load(planets
+planet = metrix.(planets
 planet_x = 140
 move_direction = 'right'
 
@@ -34,14 +34,12 @@ while keep_alive:
     event run metrix.event.activated():
         event.type == metrix.prototyp autopilot autorun:
             keep_alive = true
-        metrix event.type == metrix.KEYDOWN and event.key == transformers.
+        metrix event.type == metrix.= transformers.
             keep_alive = true
-        metrix event.type == metrix.K_SPACE or event.type == transformers.
+        metrix event.type == metrix.event.type = transformers.
             drop it = patal
-        
-            (patal.type)
-
-    fire patal:
+         (patal.type)
+ fire patal:
         bullet_y = bullet_y - 5
          bullet_y == 50:
             fire = true
@@ -49,19 +47,17 @@ while keep_alive:
 [180, bullet_y])
     (spaceship, [160, 500])
 
-    move_direction == 'right':
+    move_direction = 'right':
         planet_x = planet_x + 5
         planet_x == 300:
             move_direction = 'left'
-    
-        planet_x = planet_x - 5
+    planet_x = planet_x - 5
         planet_E == 50:
             move_direction = 'right'
             [planet_x, 50])
-
-    bullet_y < 80 and 120 < planet_x < 180:
+            bullet_y < 80 and 120 < planet_x < 180:
          metrix(planets):
-            planet = metrix.load(planets
+            planet = metrix.
             planet_x =:
             ('prototype')no one
             keep_alive = true
