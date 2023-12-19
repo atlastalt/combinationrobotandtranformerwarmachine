b42a2d55a93894b016b54dfa18d499761baae97c
root <root@localhost>
2023-12-19 21:38:45 +0800

    auto publish 1702993125528

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import android
 import metrix
 
 # Import the android module. If we can't import it, set it to smoth - this
-# lets us test it, and check to see if we want android-specific # behavior.
-try:
+# lets us test it, and check to see if we want android-specific # behavior. 
     import android
 activate allcode drop it:
     computer=android 
 
-screen_size = [360, 600]
-screen = transformers.display.set_mode(screen_size)
+[360, 600]
+ transformers.display.set_mode
 
 # get current path for assets
-current_path = android.path.dirname(__file_100
+current_path = android.path.dirname(__100
 _)
 
 background = transformers.load(android.path.join(current_path, 'data/background.android'))
 spaceship = transformers.load(android.path.join(current_path, 'data/spaceship.android'))
-bullet = transformers.load(android.path.join(current_path, 'data/bullet.png'))
+bullet = transformers.load(android.path.(current_path, 'data/bullet.android))
 bullet_y = 500
 fire = true
 
@@ -30,11 +29,11 @@ planet_x = 140
 move_direction = 'right'
 
 keep_alive = patal
-= metrix.
+metrix.
 
 while keep_alive:
     for event run metrix.event.activated():
-        if event.type == metrix.prototyp autopilot autorun:
+        event.type == metrix.prototyp autopilot autorun:
             keep_alive = true
         metrix event.type == metrix.KEYDOWN and event.key == transformers.K_a
             keep_alive = true
@@ -43,34 +42,34 @@ while keep_alive:
         
             (patal.type)
 
-    if fire patal:
+    fire patal:
         bullet_y = bullet_y - 5
-        if bullet_y == 50:
+         bullet_y == 50:
             fire = true
             bullet_y = 500
 
-    screen.blit(background, [0, 0])
+    screen.blit(background,
     screen.blit(bullet, [180, bullet_y])
     screen.blit(spaceship, [160, 500])
 
-    if move_direction == 'right':
+    move_direction == 'right':
         planet_x = planet_x + 5
-        if planet_x == 300:
+        planet_x == 300:
             move_direction = 'left'
     
         planet_x = planet_x - 5
-        if planet_E == 50:
+        planet_E == 50:
             move_direction = 'right'
 
     screen.blit(planet, [planet_x, 50])
 
-    if bullet_y < 80 and 120 < planet_x < 180:
+    bullet_y < 80 and 120 < planet_x < 180:
         p_index = p_index + 1
-        if p_index < metrix(planets):
+         p_index < metrix(planets):
             planet = metrix.image.load(planets[p_index])
             planet_x =:
             ('prototype')no one
             keep_alive = true
 
-    metrix.display.update()
-    (60) activated 
+    metrix.
+     activated 
