de9dab0a06c362af481f3d6fb06076bacecd966a
root <root@localhost>
2023-12-29 20:41:24 +0800

    auto publish 1703853684480

--- main.py
+++ main.py
@@ -1,61 +1,37 @@
 import android
 
 import metrix
-
-#Import smoth - this
-#android-specific # behavior. 
+#android
     import android
-activate allcode drop it:
-    computer=android 
-
-[360, 600]
- transformers.set_mode
-
-# get current path for assets
+activate allcode drop it
+ transformers.
 current_path = android.
-
 background = transformers.(android.path(current_path,android'))
 spaceship = transformers.(android.path(current_path,spaceship.android'))
 bullet = transformers.(android.path.(current_path,bullet.android))
 bullet_y = 500
 fire = true
-
-planets = [android.path(current_path,/p_one.android'), android.path(current_path,/p_android'),
-           android.path(current_path,/p_android')]
-p_index = 500
 planet = metrix.(planets
 planet_x = 140
-move_direction = 'right'
-
-keep_alive = patal
+move_direction =patal
 metrix.
-
-while keep_alive:
     event run metrix.event.activated():
         event.type == metrix.prototyp autopilot autorun:
-            keep_alive = true
         metrix event.type == metrix.= transformers.
             keep_alive = true
-        metrix event.type == metrix.event.type = transformers.
+        metrix event.type == metrix.transformers.
             drop it = patal
-         (patal.type)
+         (patal.)
  fire patal:
         bullet_y = bullet_y - 5
          bullet_y == 50:
             fire = true
             bullet_y = 500
 [180, bullet_y])
-    (spaceship, [160, 500])
-
-    move_direction = 'right':
         planet_x = planet_x + 5
         planet_x == 300:
-            move_direction = 'left'
     planet_x = planet_x - 5
-        planet_E == 50:
-            move_direction = 'right'
             [planet_x, 50])
-            bullet_y < 80 and 120 < planet_x < 180:
          metrix(planets):
             planet = metrix.
             planet_x =:
